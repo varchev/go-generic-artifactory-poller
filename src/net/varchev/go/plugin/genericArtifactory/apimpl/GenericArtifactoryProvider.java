79ec9ebc8f3c5e8553e9734b2760bdaa8001cc82
Aravind SV <arvind.sv@gmail.com>
2016-12-19 17:33:19 -0500

    Make plugin work with GoCD 17.1 and above

--- src/net/varchev/go/plugin/genericArtifactory/apimpl/GenericArtifactoryProvider.java
+++ src/net/varchev/go/plugin/genericArtifactory/apimpl/GenericArtifactoryProvider.java
@@ -3,7 +3,6 @@ package net.varchev.go.plugin.genericArtifactory.apimpl;
 import com.thoughtworks.go.plugin.api.annotation.Extension;
 import com.thoughtworks.go.plugin.api.material.packagerepository.PackageMaterialProvider;
 
-@Extension
 public class GenericArtifactoryProvider implements PackageMaterialProvider {
 
     public PluginConfig getConfig() {
